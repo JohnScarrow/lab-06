d59e87c3364811d8d2b013574b6c55819415a79e
John Scarrow <johna@scarrows.com>
2017-04-10 10:36:29 -0700

    good baseline

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+Console.log('shark in the dark!');
+
+//object example
+var exampleOjectThings = {
+  title: 'Thing magig',
+  date: '10/23',
+  item:['things', 'stuff', 'whatlol'],
+  author:'slugnibble',
+}
