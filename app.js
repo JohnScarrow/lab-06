66d9619ea68df7e8e968fe4155fdfea1565a4223
John Scarrow <johna@scarrows.com>
2017-04-10 12:51:46 -0700

    objects and DOM

--- app.js
+++ app.js
@@ -3,9 +3,49 @@
 Console.log('shark in the dark!');
 
 //object example
-var exampleOjectThings = {
+var exampleObjectThing = {
   title: 'Thing magig',
   date: '10/23',
   item:['things', 'stuff', 'whatlol'],
   author:'slugnibble',
+};
+
+var stuff = {
+  title: 'Thing magig',
+  date: '10/23',
+  item:['things', 'stuff', 'whatlol'],
+  author:'slugnibble',
+};
+
+var lolwut = {
+  title: 'Thing magig',
+  date: '10/23',
+  item:['things', 'stuff', 'whatlol'],
+  author:'slugnibble',
+};
+
+var thingArchives = [
+  exampleOjectThings,
+  stuff,
+  lolwut,
+];
+
+//Changing the title
+var heading = document.getElementById('title');
+heading.textContent = exampleOjectThing.title;
+heading.setAttribute('class', 'cool box button');
+
+var listListUl = document.getElementById('list')
+//Create a li tag for each string in the object
+//Fill that li with the string of the object
+//Put he new li at the end of listListUl (using appendChild)
+
+thingLi;
+for (var i = 0; i <exampleObjectThing); i++){
+  console.log('exampleObjectThing.item of i',exampleObjectThing.item[1]);
+  thingLi= document.createElement('li');
+
+  thingLi.textContent = exampleObjectThing.item[1];
+  thingLi.appendChild(thingLi);
+
 }
